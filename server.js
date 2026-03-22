cbd07741edc2c5d3d987202f4045fecb3df54735
Mateusz Wojdas <mati@MacBook-Air-Mateusz.local>
2026-03-22 13:18:29 +0100

    Initial commit: anty-adhd app

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end(html);
+}).listen(PORT, () => {
+  console.log(`anty-adhd running on port ${PORT}`);
+});
